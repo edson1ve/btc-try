61e928d8b3efa93fc9362f1ad04cab3e9c835adb
Erhan Alankus <e.alankus@gmail.com>
2019-09-04 10:52:22 +0300

    Initialize Repository with Version 0.0.0.2

---
+++ content.js
@@ -0,0 +1,5 @@
+var balance = document.getElementsByClassName('balanceli')[0];
+var wrapper = document.createElement('a');
+balance.parentNode.insertBefore(wrapper, balance);
+wrapper.appendChild(balance);
+wrapper.href = "https://www.google.com/search?q=" + document.getElementById("balance").innerHTML + "+BTC+in+TRY";
