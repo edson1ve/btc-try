01109854bcdb106c9f274c205e3e612be15dae17
edson1ve <47576165+edson1ve@users.noreply.github.com>
2019-12-21 21:00:55 -0400

    UPDATE
    FOR PRICE VENEZUELA

--- content.js
+++ content.js
@@ -2,4 +2,4 @@ var balance = document.getElementsByClassName('balanceli')[0];
 var wrapper = document.createElement('a');
 balance.parentNode.insertBefore(wrapper, balance);
 wrapper.appendChild(balance);
-wrapper.href = "https://www.google.com/search?q=" + document.getElementById("balance").innerHTML + "+BTC+in+TL";
+wrapper.href = "https://www.google.com/search?q=" + document.getElementById("balance").innerHTML + "+BTC+in+VES";
